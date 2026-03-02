54477caa30340dbcc5a89c1f07c2a53dcf1d42d8
Jack-Muoio <jackmuoio81@gmail.com>
2026-03-02 12:55:57 -0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Nav from './components/Nav';
+import Landing from './components/Landing';
+import Highlights from './components/Highlights';
+import Featured from './components/Featured';
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from "./data";
+import BooksInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
+import React, { useEffect, useState } from 'react';
+
+
 
 function App() {
+  const [cart, setCart] = useState([])
+  
+
+  function addToCart(book) {
+    setCart([...cart, {...book, quantity: 1}])
+  }
+
+  function removeFromCart(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(cart.map(item => item.id === book.id
+        ? {
+          ...item, 
+          quantity: +quantity,
+        }
+      : item
+    ))
+  }
+
+  function numberOfItems() {
+    let counter = 0
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter
+  }
+
+  useEffect(() => {
+    console.log([cart])
+  }, [cart])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router >
+        <div className="App">
+          <Nav numberOfItems={numberOfItems()}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/books" exact render={() => <Books books={books} />} />
+          <Route path="/books/:id" render={() => <BooksInfo books={books} addToCart={addToCart} cart={cart}/>} />
+          <Route path="/cart" render={() => <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeFromCart={removeFromCart}/>} />
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
